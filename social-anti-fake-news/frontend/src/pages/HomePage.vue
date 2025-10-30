8d6b10efba6637f295bee2a08b5c286a6562cdbd
Wanikkasit <wanikkasit_n@cmu.ac.th>
2025-10-31 03:09:58 +0700

    bugfix. only reader cant add news

--- social-anti-fake-news/frontend/src/pages/HomePage.vue
+++ social-anti-fake-news/frontend/src/pages/HomePage.vue
@@ -78,7 +78,7 @@
           </div>
 
           <button
-              v-if="auth.isMember"
+              v-if="auth.isMember || auth.isAdmin"
               @click="goToAddNews"
               class="px-5 py-2 bg-gradient-to-r from-green-600 to-blue-600 text-white font-semibold rounded-full shadow-md hover:opacity-90 transition-all duration-200"
           >
